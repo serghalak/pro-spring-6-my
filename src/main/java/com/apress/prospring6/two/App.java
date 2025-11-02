82474ac193030cff140fc051a81a888b0d4b2da9
Serhii <serghalak@gmail.com>
2025-11-02 11:09:42 +0200

    add ApplicationContext

--- src/main/java/com/apress/prospring6/two/App.java
+++ src/main/java/com/apress/prospring6/two/App.java
@@ -1,6 +1,8 @@
 package com.apress.prospring6.two;
 
 import com.apress.prospring6.two.decoupled.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -15,12 +17,16 @@ public class App
 //        MessageProvider mp = new HelloWorldMessageProvider();
 //        mr.setMessageProvider(mp);
 //        mr.render();
-        MessageSupportFactory msf = MessageSupportFactory.getInstance();
-        MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer()
-                .orElseThrow(() -> new IllegalArgumentException("Service of type 'MessageRenderer' was not found"));
-        MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider()
-                .orElseThrow(() -> new IllegalArgumentException("Service of type 'MessageProvider' was not found"));
-        mr.setMessageProvider(mp);
+//        MessageSupportFactory msf = MessageSupportFactory.getInstance();
+//        MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer()
+//                .orElseThrow(() -> new IllegalArgumentException("Service of type 'MessageRenderer' was not found"));
+//        MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider()
+//                .orElseThrow(() -> new IllegalArgumentException("Service of type 'MessageProvider' was not found"));
+//        mr.setMessageProvider(mp);
+//        mr.render();
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
+
     }
 }
