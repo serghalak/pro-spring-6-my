3094024fea8d5e7c8ed128a59192792c87ae8d64
Serhii <serghalak@gmail.com>
2025-11-16 13:50:09 +0200

    add ApplicationContext with annotations and xml configuration

--- src/main/java/com/apress/prospring6/two/App.java
+++ src/main/java/com/apress/prospring6/two/App.java
@@ -1,24 +1,19 @@
 package com.apress.prospring6.two;
 
-import com.apress.prospring6.two.annotated.HelloWorldConfiguration;
-import com.apress.prospring6.two.decoupled.*;
+import com.apress.prospring6.two.decoupled.MessageRenderer;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
- *
  */
 @Configuration
 @ComponentScan
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 //        MessageRenderer mr = new StandardOutMessageRenderer();
 //        MessageProvider mp = new HelloWorldMessageProvider();
 //        mr.setMessageProvider(mp);
@@ -33,9 +28,12 @@ public class App
 //        ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
 //        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
 //        mr.render();
-       //ApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-       ApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
-       MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
-       mr.render();
+        //ApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        //ApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
     }
 }
--- src/main/java/com/apress/prospring6/two/decoupled/HelloWorldMessageProvider.java
+++ src/main/java/com/apress/prospring6/two/decoupled/HelloWorldMessageProvider.java
@@ -1,11 +1,21 @@
 package com.apress.prospring6.two.decoupled;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component("provider")
+//@Component("provider")
 public class HelloWorldMessageProvider implements MessageProvider {
+
+    private String message;
+
+    //@Autowired
+    public HelloWorldMessageProvider(/*@Value("Configurable message")*/ String message) {
+        this.message = message;
+    }
+
     @Override
     public String getMessage() {
-        return "Hello World!";
+        return message; /*"Hello World!";*/
     }
 }
